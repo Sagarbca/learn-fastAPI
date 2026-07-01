a2859c9d08ac16a45308d746a68d78d664f47e95
sagar <sagar+mmnova@mmnovatech.com>
2026-07-01 17:39:18 +0530

    get params issues

--- main.py
+++ main.py
@@ -22,3 +22,20 @@ async def get_users():
 @app.get("/user/{user_id}")
 async def get_user(user_id: int):
     return {"user_id": user_id, "name": f"User {user_id}"}
+
+#optional routes
+@app.get("/items")
+async def get_items(item : str = "None"):
+    return {"item": item}
+
+
+#default route
+@app.get("/products")
+async def get_products(limit : int = 10):
+    return {"products": ["Product 1", "Product 2", "Product 3"], "limit": limit}
+
+#multiple query parameters
+@app.get("/productsWithLimitOffset")
+async def get_products(limit : int = 10, offset : int = 0):
+    return {"products": ["Product 1", "Product 2", "Product 3"], "limit": limit, "offset": offset}
+
