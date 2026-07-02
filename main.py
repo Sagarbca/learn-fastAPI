3105b4867a062bd9feeb61e023a64b1e5dd0649b
sagar <41232163+Sagarbca@users.noreply.github.com>
2026-07-02 17:20:02 +0530

    Update main.py
    add middleware terminoly and response concept

--- main.py
+++ main.py
@@ -1,69 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from pydantic import BaseModel
-
+import time
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return {"Hello": "World"}
-
-#about
-@app.get("/about")
-async def about():
-    return {"About": "This is a FastAPI application."}
-
-
-#users
-@app.get("/users")
-async def get_users():
-    return {"users": ["Alice", "Bob", "Charlie"]}
-
-
-#dynamic users route
-@app.get("/user/{user_id}")
-async def get_user(user_id: int):
-    return {"user_id": user_id, "name": f"User {user_id}"}
-
-#optional routes
-@app.get("/items")
-async def get_items(item : str = "None"):
-    return {"item": item}
-
-
-#default route
-@app.get("/products")
-async def get_products(limit : int = 10):
-    return {"products": ["Product 1", "Product 2", "Product 3"], "limit": limit}
-
-#multiple query parameters
-@app.get("/productsWithLimitOffset")
-async def get_products(limit : int = 10, offset : int = 0):
-    return {"products": ["Product 1", "Product 2", "Product 3"], "limit": limit, "offset": offset}
-
-
-#handle pydantic and post route
-@app.post("/create_user")
-def create_user(name: str, age: int):
-    return {"message": "User created", "user": {"name": name, "age": age}}
-
-
-@app.post("/create_user_with_json")
-def create_user_pydantic(user: dict):
-    return {"message": "User created", "user": user}
-
-class Address(BaseModel):
-    street: str
-    city: str
-    state: str
-    zip_code: str
-
-class User(BaseModel):
-    name: str
-    age: int
-    email: str
-    address: Address
-
-
-@app.post("/create_user_with_pydantic_model")
-def create_user_with_pydantic(user: User):
-    return {"message": "User created", "user": user}
+@app.middleware("http")
+async def my_middleware(request: Request, call_next):
+    # Code to execute before the request is processed
+    start_time = time.time()
+    print("Before request")
+    
+    response = await call_next(request)
+    
+    # Code to execute after the request is processed
+    end_time = time.time()
+    print(f"After request: {end_time - start_time} seconds")
+    
+    return response
