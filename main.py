780029e10d5a15216356b39ddcbf90414ae1c892
sagar <87705393+sagar-peregrine@users.noreply.github.com>
2026-07-02 12:58:33 +0530

    Update main.py
    add response models

--- main.py
+++ main.py
@@ -3,67 +3,25 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-async def home():
-    return {"Hello": "World"}
-
-#about
-@app.get("/about")
-async def about():
-    return {"About": "This is a FastAPI application."}
-
-
-#users
-@app.get("/users")
-async def get_users():
-    return {"users": ["Alice", "Bob", "Charlie"]}
-
-
-#dynamic users route
-@app.get("/user/{user_id}")
-async def get_user(user_id: int):
-    return {"user_id": user_id, "name": f"User {user_id}"}
-
-#optional routes
-@app.get("/items")
-async def get_items(item : str = "None"):
-    return {"item": item}
-
-
-#default route
-@app.get("/products")
-async def get_products(limit : int = 10):
-    return {"products": ["Product 1", "Product 2", "Product 3"], "limit": limit}
-
-#multiple query parameters
-@app.get("/productsWithLimitOffset")
-async def get_products(limit : int = 10, offset : int = 0):
-    return {"products": ["Product 1", "Product 2", "Product 3"], "limit": limit, "offset": offset}
-
-
-#handle pydantic and post route
-@app.post("/create_user")
-def create_user(name: str, age: int):
-    return {"message": "User created", "user": {"name": name, "age": age}}
-
-
-@app.post("/create_user_with_json")
-def create_user_pydantic(user: dict):
-    return {"message": "User created", "user": user}
-
-class Address(BaseModel):
-    street: str
-    city: str
-    state: str
-    zip_code: str
+users = []
 
 class User(BaseModel):
     name: str
-    age: int
     email: str
-    address: Address
+    password: str
+class UserResponse(BaseModel):
+    name: str
+    email: str
+    full_name: str
 
 
-@app.post("/create_user_with_pydantic_model")
-def create_user_with_pydantic(user: User):
-    return {"message": "User created", "user": user}
+@app.get("/users/", response_model=UserResponse)
+async def get_user(user: User):
+    return UserResponse(name=user.name, email=user.email, full_name=f"{user.name} <{user.email}>")
+
+
+# store user data in memory
+@app.post("/users/", response_model=UserResponse)
+async def create_user(user: User):
+    users.append(user)
+    return UserResponse(name=user.name, email=user.email, full_name=f"{user.name} <{user.email}>")
